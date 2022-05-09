0c70fb0170efee4099a7bfb6d575e7e436fba0ee
Zytro <65337921+ZytroCode@users.noreply.github.com>
2022-05-09 15:59:35 +0800

    Add init function

--- src/pypure/core/__init__.py
+++ src/pypure/core/__init__.py
@@ -2,7 +2,9 @@ import pypure
 
 
 class Core(sys.modules[__name__].__class__):
-    ...
+
+    def init(self, config=True, config_path=f"{os.getcwd()}/pure.ini"):
+        """Initializes pypure and it's components"""
 
 
 # Override the core module
