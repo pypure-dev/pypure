7c000bff8bcfe49dfeeec167d09325ad1b2708da
Zytro <65337921+ZytroCode@users.noreply.github.com>
2022-05-09 16:04:36 +0800

    Update pypure.core.init

--- src/pypure/core/__init__.py
+++ src/pypure/core/__init__.py
@@ -6,9 +6,13 @@ class Core(sys.modules[__name__].__class__):
     def init(self, config=True, config_path=f"{os.getcwd()}/pure.ini"):
         """Initializes pypure and it's components"""
 
+        # Initializing pygame
+        self.pygame = __import__("pygame")
+        self.pygame.init()
 
     def quit(self):
         """Uninitializes and resets pypure"""
+
 
 # Override the core module
 sys.modules[__name__].__class__ = Core
