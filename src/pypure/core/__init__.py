a96942f64ba9dd151e2971dfe43b7ea76407a3d7
Zytro <65337921+ZytroCode@users.noreply.github.com>
2022-05-12 16:23:30 +0800

    Rewrite pypure.core

--- src/pypure/core/__init__.py
+++ src/pypure/core/__init__.py
@@ -4,63 +4,66 @@ import sys
 
 from configparser import ConfigParser
 
+# Used for setting global variables
+__self__ = sys.modules[__name__]
 
-class Core(sys.modules[__name__].__class__):
-    debug = None
-    pygame = sys.modules[__name__].__class__("pygame")
-    config = ConfigParser()
-    config.user = None
-    config.default = f"{os.path.dirname(__file__)}/pure.ini"
-
-    def init(self, config=True, config_path=f"{os.getcwd()}/pure.ini"):
-        """Initializes pypure and it's components"""
-
-        # Correcting config parameter
-        if not os.path.exists(config_path) and config:
-            config = False
-
-        # Reading configuration files
-        if config:
-            self.config.read([
-                config_path,  # User's configuration file
-                self.config.default,  # Default configuration file
-            ])
-        else:
-            self.config.read(self.config.default)  # Default configuration file
-            self.config.user = config_path
-
-        # Applying configuration
-        self.config.pure = self.config["pure"]
-        self.config.pygame = self.config["tools.pygame"]
-
-        self.debug = self.config.pure.getboolean("debug")
-        pygame_support_prompt = self.config.pygame.getboolean("support_prompt")
-
-        if not pygame_support_prompt:
-            os.environ["PYGAME_HIDE_SUPPORT_PROMPT"] = "1"
-
-        # Initializing pygame
-        self.pygame = __import__("pygame")
-        self.pygame.support_prompt = pygame_support_prompt
-        self.pygame.init()
-
-        from pygame import _sdl2 as SDL2
-        self.pygame.SDL2 = SDL2
-
-        # DEBUG
-        if self.debug:
-            print(f"[Pure] Version: {pypure.__version__}")
-            print(f"[Pure] Config: {config}")
-
-    def quit(self):
-        """Uninitializes and resets pypure"""
-
-        # Uninitializing pygame
-        self.pygame.quit()
-
-        # Resetting core
-        self = self.__class__(__name__)
+# Variables
+debug = None
+pygame = sys.modules[__name__].__class__("pygame")
+config = ConfigParser()
+config.user = None
+config.default = f"{os.path.dirname(__file__)}/pure.ini"
 
 
-# Override the core module
-sys.modules[__name__].__class__ = Core
+def init(
+    config=True,
+    config_path=config.default,
+):
+    """Initializes pypure and it's components"""
+
+    # Correcting config parameter
+    if not os.path.exists(config_path) and config:
+        config = False
+
+    # Reading configuration files
+    if config:
+        __self__.config.read([
+            config_path,  # User's configuration file
+            __self__.config.default,  # Default configuration file
+        ])
+    else:
+        __self__.config.read(__self__.config.default)  # Default configuration file
+        __self__.config.user = config_path
+
+    # Applying configuration
+    __self__.config.pure = __self__.config["pure"]
+    __self__.config.pygame = __self__.config["tools.pygame"]
+
+    __self__.debug = __self__.config.pure.getboolean("debug")
+    pygame_support_prompt = __self__.config.pygame.getboolean("support_prompt")
+
+    if not pygame_support_prompt:
+        os.environ["PYGAME_HIDE_SUPPORT_PROMPT"] = "1"
+
+    # Initializing pygame
+    __self__.pygame = __import__("pygame")
+    __self__.pygame.support_prompt = pygame_support_prompt
+    __self__.pygame.init()
+
+    from pygame import _sdl2 as SDL2
+    __self__.pygame.SDL2 = SDL2
+
+    # DEBUG
+    if __self__.debug:
+        print(f"[Pure] Version: {pypure.__version__}")
+        print(f"[Pure] Config: {config}")
+
+
+def quit():
+    """Uninitializes and resets pypure"""
+
+    # Uninitializing pygame
+    __self__.pygame.quit()
+
+    # Resetting core
+    sys.modules[__name__] = __self__.__class__(__name__)
